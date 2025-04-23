826ccb84e8a68a29a622f95fd5109b8f6b302d0f
stillbencabal <cabalstillben@gmail.com>
2025-04-23 16:32:37 -0700

    Removed unnecessary parts of highlight yank

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -12,8 +12,6 @@ vim.opt.scrolloff = 8
 vim.opt.guicursor = ""
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true}),
     callback = function()
     vim.highlight.on_yank()
     end,
