33c43fed002fb63ba18192932fd6f4c6e3be95dc
stillbencabal <cabalstillben@gmail.com>
2025-04-29 09:03:02 -0700

    Added end of file alignment shortcut

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,5 +1,6 @@
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "G", "Gzz")
 vim.cmd("set number relativenumber")
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
