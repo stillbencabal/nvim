07b6a2eff39aafadcbefb07900bb5ee59442bf2b
stillbencabal <cabalstillben@gmail.com>
2026-01-02 13:48:59 -0800

    Added Kotlin LSP

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pylsp" }
+                ensure_installed = { "lua_ls", "pylsp", "kotlin_language_server" }
             })
         end
     },
@@ -18,6 +18,7 @@ return {
         config = function()
             vim.lsp.config("lua_ls.setup",{})
             vim.lsp.config("pylsp.setup",{})
+            vim.lsp.config("kotlin_language_server.setup",{})
             vim.keymap.set('n', '<C-k>', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
         end
