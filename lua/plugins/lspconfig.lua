bba4d59517906dc9b325cc5091487474bc11e5d5
stillbencabal <cabalstillben@gmail.com>
2026-01-17 00:10:51 -0800

    Ensure clangd installed and added LSP diagnostics

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pylsp", "kotlin_lsp" }
+                ensure_installed = { "lua_ls", "pylsp", "kotlin_lsp", "clangd"}
             })
         end
     },
@@ -19,6 +19,7 @@ return {
             vim.lsp.config("lua_ls.setup",{})
             vim.lsp.config("pylsp.setup",{})
             vim.lsp.config("kotlin_lsp.setup",{})
+            vim.diagnostic.config({virtual_text = true})
             vim.keymap.set('n', '<C-k>', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
         end
