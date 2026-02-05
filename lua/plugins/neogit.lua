82557f9a3896d45d2a0bcd3ce14428d8fced28d1
stillbencabal <cabalstillben@gmail.com>
2026-02-05 12:34:57 -0800

    Created Neogit shortcut

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -3,5 +3,13 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope.nvim"
-    }
+    },
+    config = function()
+        local neogit = require('neogit')
+        vim.keymap.set(
+            'n',
+            '<C-g>',
+            function() neogit.open({kind = "vsplit" }) end,
+            { desc = "Open Neogit UI in another window" })
+    end
 }
